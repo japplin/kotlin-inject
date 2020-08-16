c6f248de2e5b15dbd82be66379db4d2a5f595ed2
Evan Tatarka <evan@tatarka.me>
2020-08-16 17:50:24 -0400

    Migrate tests to use compile testing

--- compiler-tests/src/test/kotlin/me/tatarka/inject/test/ProjectCompiler.kt
+++ compiler-tests/src/test/kotlin/me/tatarka/inject/test/ProjectCompiler.kt
@@ -1,119 +1,60 @@
 package me.tatarka.inject.test
 
 import assertk.Assert
-import assertk.assertions.isInstanceOf
-import org.gradle.testkit.runner.GradleRunner
-import org.gradle.testkit.runner.UnexpectedBuildFailure
+import assertk.assertions.isNotNull
+import assertk.assertions.message
+import com.tschuchort.compiletesting.*
+import me.tatarka.inject.compiler.kapt.InjectCompiler
+import me.tatarka.inject.compiler.kapt.ScopedInjectCompiler
+import me.tatarka.inject.compiler.ksp.InjectProcessor
+import org.jetbrains.kotlin.ksp.processing.SymbolProcessor
 import java.io.File
+import javax.annotation.processing.Processor
 
 class ProjectCompiler(private val root: File, private val target: Target) {
 
-    private lateinit var sourceDir: File
+    private val sourceFiles = mutableListOf<SourceFile>()
 
     fun setup(): ProjectCompiler {
-        val kotlinVersion = if (target == Target.ksp) "1.4.0-rc" else "1.3.70"
-
-        val settingsFile = root.resolve("settings.gradle")
-
-        val pluginManagement = """
-           pluginManagement {
-               repositories {
-                   google()
-                   maven { url "https://dl.bintray.com/kotlin/kotlin-eap" }
-                   mavenCentral()
-                   gradlePluginPortal()
-               }
-               resolutionStrategy {
-                   eachPlugin {
-                       switch (requested.id.id) {
-                           case "kotlin-ksp":
-                           case "org.jetbrains.kotlin.kotlin-ksp":
-                           case "org.jetbrains.kotlin.ksp":
-                               useModule("org.jetbrains.kotlin:kotlin-ksp:" + requested.version)
-                       }
-                   }
-               }
-           }
-        """.trimIndent()
-
-        settingsFile.writeText(
-            """
-            ${if (target == Target.ksp) pluginManagement else ""}
-           
-           include ':kotlin-inject-compiler-core'
-           include ':kotlin-inject-compiler-${target}'
-           include ':kotlin-inject-runtime'
-           include ':compiler-test'
-           project(':kotlin-inject-compiler-core').projectDir = new File('${File("../kotlin-inject-compiler-core").absolutePath}')
-           project(':kotlin-inject-compiler-${target}').projectDir = new File('${File("../kotlin-inject-compiler-${target}").absolutePath}')
-           project(':kotlin-inject-runtime').projectDir = new File('${File("../kotlin-inject-runtime").absolutePath}')
-        """.trimIndent()
-        )
-
-        val rootBuildFile = root.resolve("build.gradle")
-
-        rootBuildFile.writeText(
-            """
-            allprojects {
-                repositories {
-                    google()
-                    maven { url "https://dl.bintray.com/kotlin/kotlin-eap" }
-                    mavenCentral()
-                }
-            }
-            """.trimIndent()
-        )
-
-        val dir = root.resolve("compiler-test")
-        dir.mkdirs()
-
-        val buildFile = dir.resolve("build.gradle")
-
-        val pluginVersion = if (target == Target.ksp) "1.4.0-rc-dev-experimental-20200814" else kotlinVersion
-
-        buildFile.writeText(
-            """
-            plugins {
-                id 'org.jetbrains.kotlin.jvm' version '${kotlinVersion}'
-                id("org.jetbrains.kotlin.$target") version '${pluginVersion}'
-            }
-            
-            dependencies {
-                implementation 'org.jetbrains.kotlin:kotlin-stdlib'
-                $target project(':kotlin-inject-compiler-${target}')
-                implementation project(':kotlin-inject-runtime')
-            }
-            """.trimIndent()
-        )
-
-        sourceDir = dir.resolve("src/main/kotlin")
-        sourceDir.mkdirs()
-
         return this
     }
 
     fun source(fileName: String, source: String): ProjectCompiler {
-        val sourceFile = sourceDir.resolve(fileName)
-        sourceFile.writeText(source)
+        sourceFiles.add(
+            SourceFile.kotlin(
+                fileName,
+                source
+            )
+        )
         return this
     }
 
     fun clear() {
-        sourceDir.recursiveDelete()
-        sourceDir.mkdirs()
+        sourceFiles.clear()
     }
 
     fun compile() {
-        GradleRunner.create()
-            .withProjectDir(root)
-            .withArguments("assemble")
-            .build()
+        val result = KotlinCompilation().apply {
+            sources = sourceFiles
+            inheritClassPath = true
+            workingDir = root
+            when (target) {
+                Target.kapt -> {
+                    annotationProcessors = listOf<Processor>(InjectCompiler(), ScopedInjectCompiler())
+                }
+                Target.ksp -> {
+                    symbolProcessors = listOf<SymbolProcessor>(InjectProcessor())
+                }
+            }
+        }.compile()
+
+        if (result.exitCode != KotlinCompilation.ExitCode.OK) {
+            throw Exception(result.messages)
+        }
     }
 }
 
-fun Assert<Throwable>.output() = isInstanceOf(UnexpectedBuildFailure::class).transform { actual ->
-    actual.buildResult.output
-}
+fun Assert<Throwable>.output(): Assert<String> = message().isNotNull()
 
 enum class Target {
     kapt, ksp
